8cb3472ec41cc3da8d359dd255c0b5464bfb7502
Kuldeep Kasaudhan <59829843+Kuldeep8853@users.noreply.github.com>
2020-01-13 19:01:24 +0530

    [Add functional program ]

---
+++ FunctionalPrograms/FunctionalPrograms/LeapYearProgram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FunctionalPrograms{
+    public class LeapYearProgram{
+        public static void FindLeapYear(){
+            Console.WriteLine("Enter the year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0)
+                Console.WriteLine("It is leap year ");
+            else
+                Console.WriteLine("It is not a leap year");
+        }
+    }
+}
