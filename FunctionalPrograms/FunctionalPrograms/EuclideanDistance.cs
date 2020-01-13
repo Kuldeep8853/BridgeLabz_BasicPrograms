2899dfc7c743a7cfb1708b1a2159ea6c1d11e37a
Kuldeep Kasaudhan <59829843+Kuldeep8853@users.noreply.github.com>
2020-01-13 19:04:55 +0530

    [Kuldeep]add.Implemetation of functional program

--- FunctionalPrograms/FunctionalPrograms/EuclideanDistance.cs
+++ FunctionalPrograms/FunctionalPrograms/EuclideanDistance.cs
@@ -9,7 +9,7 @@ namespace FunctionalPrograms{
             int y1 = Convert.ToInt32(Console.ReadLine());
             // Calculating distance 
             double distance= Math.Sqrt(Math.Pow(x1, 2) +Math.Pow(y1, 2) * 1.0);
-            Console.WriteLine(Math.Round(distance * 100000.0) / 100000.0);
+            Console.WriteLine(Math.Round(distance * 100000.00) / 100000.00);
         }
 
     }
