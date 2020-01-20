a9f1974233832173b9301984bf07f9655b108f62
Kuldeep Kasaudhan <59829843+Kuldeep8853@users.noreply.github.com>
2020-01-20 18:34:58 +0530

    add Logical programs

---
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the your choice: ");
+            Console.WriteLine("1. Gambler game. ");
+            Console.WriteLine("2. Coupon Numbers Program. ");
+            Console.WriteLine("3. Simulate Stopwatch Program. ");
+            Console.WriteLine("4. Tic tac Toe game. ");
+            int choice= Convert.ToInt32(Console.ReadLine());
+            switch (choice){
+                case 1:{
+                        Gambler.FindWinLos();
+                        break;
+                    }
+                case 2:{
+                        CouponNumbers.GenerateCouponNumbers();
+                        break;
+                    }
+                case 3:
+                    {
+                        StopwatchProgram.GetElapsedTime();
+                        break;
+                    }
+                case 4:
+                    {
+                        TicTacToeGame.FindWhoWin();
+                        break;
+                    }
+                default: 
+                    {
+                        Console.WriteLine("Please select valid choice.");
+                        break;
+                    }
+            }
+        }
+    }
+}
