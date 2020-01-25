ad676e4f620bfe6cffaffc48f13298d26187d728
Kuldeep Kasaudhan <59829843+Kuldeep8853@users.noreply.github.com>
2020-01-25 13:19:36 +0530

    Revert "Revert "Revert "Revert "[Kuldeep]Add. programs""""
    This reverts commit 4aaf393fad6473a88170042119dcac11c245e916.

---
+++ Functional/FunctionalPrograms/FunctionalPrograms/TwoDArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionalPrograms
+{
+    class TwoDArray{
+		public static void ArrayInt(){
+			Console.WriteLine("Enter Row and Column:");
+			int m = Convert.ToInt32(Console.ReadLine());
+			int n = Convert.ToInt32(Console.ReadLine());
+			int[,] a = new int[m, n];
+			Console.WriteLine();
+			Console.WriteLine("Enter the Array elements.");
+	
+			for(int i = 0; i<m; i++)
+			{
+				for (int j = 0; j<n; j++)
+			    {
+					a[i,j] = Convert.ToInt32(Console.ReadLine());
+				}
+            }
+			for (int i = 0; i < m; i++)
+			{
+				for (int j = 0; j < n; j++)
+				{
+					Console .Write(a[i, j]+" ");
+				}
+				Console.WriteLine();
+			}
+		}
+		
+    }
+}
