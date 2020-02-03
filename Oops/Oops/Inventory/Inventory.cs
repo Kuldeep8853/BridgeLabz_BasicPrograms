33ac1a7e17e5181bbc604ea356fed8a9a6f42806
Kuldeep Kasaudhan <59829843+Kuldeep8853@users.noreply.github.com>
2020-02-03 16:27:03 +0530

    [Kuldeep]add.Delate and update methods

--- Oops/Oops/Inventory/Inventory.cs
+++ Oops/Oops/Inventory/Inventory.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-
+﻿
 namespace Oops.Inventory{
-   public class Inventory{
+    /// <summary>
+    /// Inventory item class.....
+    /// </summary>
+    public class Inventory{
+        /// <summary>
+        /// name of the item.......
+        /// </summary>
         public string Name { set; get; }
+        /// <summary>
+        /// Price of the item.......
+        /// </summary>
         public int Price { set; get; }
+        /// <summary>
+        /// weight of the items......
+        /// </summary>
         public int Weight { set; get; }
     }
    
