21324db267b9029fcc2a5e79b3c7d7118ab313bb
Kuldeep Kasaudhan <59829843+Kuldeep8853@users.noreply.github.com>
2020-02-04 16:42:16 +0530

    [Kuldeep]Modified.Add update and Delate method.

--- Oops/Oops/StockAccountManagement/StockAccountDriver.cs
+++ Oops/Oops/StockAccountManagement/StockAccountDriver.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json.Linq;
 using System;
 using System.IO;
+using System.Linq;
 
 namespace Oops.StockAccountManagement
 {
@@ -190,26 +191,225 @@ namespace Oops.StockAccountManagement
         /// </summary>
         public static void UpdateStock()
         {
+            ////Read the json file.....
             string files = File.ReadAllText(@"D:\Oops\Oops\StockAccountManagement\CompanyInJsonFile.json");
             var jObject = JObject.Parse(files);
-            JArray RiceArrary = (JArray)jObject["Rice"];
-            Console.Write("Enter rice price : ");
-            int price = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter rice weight : ");
-            int weight = Convert.ToInt32(Console.ReadLine());
-            if (price > 0 && weight > 0)
-            {
-                Console.Write("Enter the rice name : ");
-                var name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter the choice Update the details......");
+            Console.WriteLine("Amazon. Update the share name of given index");
+            Console.WriteLine("Flipkart. Update the share name of given index");
+            Console.WriteLine("Tcs. Update the share name of given index");
+            string choice = Console.ReadLine();
+            switch (choice)
+            { 
+                //// Update the amazon array....
+                case "Amazon":
+                    {
+                        JArray AmazonArrary = (JArray)jObject["Amazon"];
+                        // int length = ((JArray)jObject["Amazon"]).Count;====>Find Amazon array Length.
+                        //Console.WriteLine(length);
+                        Console.WriteLine("Enter the Amazon array index");
+                        int index = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the choice Update the details......");
+                        Console.WriteLine("ShareName.  Update the share name of given index");
+                        Console.WriteLine("NoOfShare.  Update the number of share of given index");
+                        Console.WriteLine("SharePrice. Update the share price of given index");
+                        string choice1 = Console.ReadLine();
+                        switch (choice1)
+                        {
+                            //// Update the share name........
+                            case "ShareName":
+                                {
+                                    Console.Write("Enter the stock name : ");
+                                    string name = Convert.ToString(Console.ReadLine());
+                                    AmazonArrary[index]["ShareName1"] = name;
+                                    break;
+                                }
+                                ////Update the share number.....
+                            case "NoOfShare":
+                                {
+                                    Console.Write("Enter the number of stock : ");
+                                    int n = Convert.ToInt32(Console.ReadLine());
+                                    AmazonArrary[index]["NoOfShare1"] = n;
+                                    break;
+                                }
+                                ////Update the price of the share......
+                            case "SharePrice":
+                                {
+                                    Console.Write("Enter the stock price : ");
+                                    int price = Convert.ToInt32(Console.ReadLine());
+                                    AmazonArrary[index]["SharePrice1"] = price;
+                                    break;
+                                }
+                        }
+                        jObject["Amazon"] = AmazonArrary;
+                        break;
+                    }
+                    ////update the Flipkart array of the object.....
+                case "Flipkart":
+                    {
+                        JArray FlipkartArrary = (JArray)jObject["Flipkart"];
+                        // int length = ((JArray)jObject["Amazon"]).Count;====>Find Amazon array Length.
+                        //Console.WriteLine(length);
+                        Console.WriteLine("Enter the Flipkart array index");
+                        int index = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the choice Update the details......");
+                        Console.WriteLine("ShareName.  Update the share name of given index");
+                        Console.WriteLine("NoOfShare.  Update the number of share of given index");
+                        Console.WriteLine("SharePrice. Update the share price of given index");
+                        string choice1 = Console.ReadLine();
+                        switch (choice1)
+                        {
+                            //// Update the share name........
+                            case "ShareName":
+                                {
+                                    Console.Write("Enter the stock name : ");
+                                    string name = Convert.ToString(Console.ReadLine());
+                                    FlipkartArrary[index]["ShareName1"] = name;
+                                    break;
+                                }
+                            ////Update the share number.....
+                            case "NoOfShare":
+                                {
+                                    Console.Write("Enter the number of stock : ");
+                                    int n = Convert.ToInt32(Console.ReadLine());
+                                    FlipkartArrary[index]["NoOfShare1"] = n;
+                                    break;
+                                }
+                            ////Update the price of the share......
+                            case "SharePrice":
+                                {
+                                    Console.Write("Enter the stock price : ");
+                                    int price = Convert.ToInt32(Console.ReadLine());
+                                    FlipkartArrary[index]["SharePrice1"] = price;
+                                    break;
+                                }
+                        }
+                        jObject["Flipkart"] = FlipkartArrary;
+                        break;
+                    }
+                    ////Update the Tcs array object......
+                case "Tcs":
+                    {
+                        JArray TcsArrary = (JArray)jObject["Tcs"];
+                        // int length = ((JArray)jObject["Amazon"]).Count;====>Find Amazon array Length.
+                        //Console.WriteLine(length);
+                        Console.WriteLine("Enter the Tcs array index");
+                        int index = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the choice Update the details......");
+                        Console.WriteLine("ShareName.  Update the share name of given index");
+                        Console.WriteLine("NoOfShare.  Update the number of share of given index");
+                        Console.WriteLine("SharePrice. Update the share price of given index");
+                        string choice1 = Console.ReadLine();
+                        switch (choice1)
+                        {
+                            //// Update the share name........
+                            case "ShareName":
+                                {
+                                    Console.Write("Enter the stock name : ");
+                                    string name = Convert.ToString(Console.ReadLine());
+                                    TcsArrary[index]["ShareName1"] = name;
+                                    break;
+                                }
+                            ////Update the price of the share......
+                            case "NoOfShare":
+                                {
+                                    Console.Write("Enter the number of stock : ");
+                                    int n = Convert.ToInt32(Console.ReadLine());
+                                    TcsArrary[index]["NoOfShare1"] = n;
+                                    break;
+                                }
+                            ////Update the price of the share......
+                            case "SharePrice":
+                                {
+                                    Console.Write("Enter the stock price : ");
+                                    int price = Convert.ToInt32(Console.ReadLine());
+                                    TcsArrary[index]["SharePrice1"] = price;
+                                    break;
+                                }
+                        }
+                        jObject["Tcs"] = TcsArrary;
+                        break;
+                    }
+            }
+                   string output = JsonConvert.SerializeObject(jObject, Formatting.Indented);
+                  File.WriteAllText(@"D:\Oops\Oops\StockAccountManagement\CompanyInJsonFile.json", output);
+        }
 
-                foreach (var item in RiceArrary)
-                {
-                    item["name"] = !string.IsNullOrEmpty(name) ? name : "";
-                }
+        /// <summary>
+        /// Remove the perticular company stock details.....
+        /// </summary>
+        public static void DelateStockDetails()
+        {
+            string files = File.ReadAllText(@"D:\Oops\Oops\StockAccountManagement\CompanyInJsonFile.json");
+            var jObject = JObject.Parse(files);
+            Console.WriteLine("enter the choice and Delate the item...");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Amazon. Delate the Amazon stock details..");
+            Console.WriteLine("Flipkart. Delate the rice item");
+            Console.WriteLine("Tcs. Delate the rice item");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {////Rice items Delations......
+                case "Amazon":
+                    {
 
-                jObject["Rice"] = RiceArrary;
-                string output = JsonConvert.SerializeObject(jObject, Formatting.Indented);
-                File.WriteAllText(@"D:\Oops\Oops\StockAccountManagement\CompanyInJsonFile.json", output);
+                        try
+                        {
+                            JArray AmazonArrary = (JArray)jObject["Amazon"];
+                            Console.Write("Enter the stock name and delate the stock details : ");
+                            string name = Console.ReadLine();
+                            //string Name = string.Empty;
+                            var DetailToDeleted = AmazonArrary.FirstOrDefault(obj => obj["ShareName1"].Value<string>() == name);
+                            AmazonArrary.Remove(DetailToDeleted);
+                            string output = JsonConvert.SerializeObject(jObject, Formatting.Indented);
+                            File.WriteAllText(@"D:\Oops\Oops\StockAccountManagement\CompanyInJsonFile.json", output);
+                        }
+                        catch (Exception)
+                        {
+                            throw;
+                        }
+                        break;
+                    }
+                case "Flipkart":
+                    {
+
+                        try
+                        {
+                            JArray FlipkartArrary = (JArray)jObject["Flipkart"];
+                            Console.Write("Enter the stock name and delate the stock details : ");
+                            string name = Console.ReadLine();
+                            //string Name = string.Empty;
+                            var DetailToDeleted = FlipkartArrary.FirstOrDefault(obj => obj["ShareName1"].Value<string>() == name);
+                            FlipkartArrary.Remove(DetailToDeleted);
+                            string output = JsonConvert.SerializeObject(jObject, Formatting.Indented);
+                            File.WriteAllText(@"D:\Oops\Oops\StockAccountManagement\CompanyInJsonFile.json", output);
+                        }
+                        catch (Exception)
+                        {
+                            throw;
+                        }
+                        break;
+                    }
+                case "Tcs":
+                    {
+
+                        try
+                        {
+                            JArray TcsArrary = (JArray)jObject["Tcs"];
+                            Console.Write("Enter the stock name and delate the stock details : ");
+                            string name = Console.ReadLine();
+                            //string Name = string.Empty;
+                            var DetailToDeleted = TcsArrary.FirstOrDefault(obj => obj["ShareName1"].Value<string>() == name);
+                            TcsArrary.Remove(DetailToDeleted);
+                            string output = JsonConvert.SerializeObject(jObject, Formatting.Indented);
+                            File.WriteAllText(@"D:\Oops\Oops\StockAccountManagement\CompanyInJsonFile.json", output);
+                        }
+                        catch (Exception)
+                        {
+                            throw;
+                        }
+                        break;
+                    }
 
             }
         }
